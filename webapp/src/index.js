b874f1571b5898c7b230959dca6dac981f5c7b8c
Joram Wilander <jwawilander@gmail.com>
2019-10-01 08:48:06 -0400

    Add frontend portion of the plugin (#2)

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,10 @@
 import {id as pluginId} from './manifest';
+import Root from './components/root';
 
 export default class Plugin {
     // eslint-disable-next-line no-unused-vars
-    initialize(registry, store) {
-        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+    initialize(registry) {
+        registry.registerRootComponent(Root);
     }
 }
 
